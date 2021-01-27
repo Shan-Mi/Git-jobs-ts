ec905fa194f495adea3ad9c8a479dbfffddc2864
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-01-27 11:22:09 +0100

    Snapshot test for rendering of App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+import App from "./App";
+
+it("renders the component", () => {
+  const component = shallow(<App />);
+  expect(component).toMatchSnapshot();
+});
